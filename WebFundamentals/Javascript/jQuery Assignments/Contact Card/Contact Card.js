f388c62fb8e7888e2ca0cb29b956ee8483c51743
Jonathan Lee <jsalinlee@gmail.com>
2017-02-05 00:53:30 -0800

    Revised Contact Card assignment. Added jsCircles revision, Pacman, and 1942.

--- WebFundamentals/Javascript/jQuery Assignments/Contact Card/Contact Card.js
+++ WebFundamentals/Javascript/jQuery Assignments/Contact Card/Contact Card.js
@@ -1,9 +1,11 @@
+var cardCount = 1;
 $("form").submit(function(){
     var firstName = $("#first-name").val();
     var lastName = $("#last-name").val();
     var description = $("#description-input").val();
-    $(".card-area").append("<div class='contact-card'><h2>" + firstName + " " + lastName + "</h2><div class='description'>" + description + "</div></div>")
-    $(".description").hide();
+    cardCount++;
+    $(".card-area").append("<div class='contact-card'><h2>" + firstName + " " + lastName + "</h2><div id='" + cardCount + "' class='description'>" + description + "</div></div>");
+    $("#" + cardCount).hide();
     return false;
 });
 $(document).on("click", ".contact-card", function(){
